fe7170f739bb126cc7ef8e420413b4b0658491fb
zidanesc <zidankren02@gmail.com>
2022-04-10 14:18:48 +0700

    feat: add animation when copying the emoji

--- src/components/EmojiBox/index.jsx
+++ src/components/EmojiBox/index.jsx
@@ -1,14 +1,29 @@
+import { useState, useEffect } from 'react'
+
 import propTypes from 'prop-types'
+import classnames from 'classnames'
 
 import styles from './EmojiBox.module.css'
 
 const EmojiBox = ({ title, symbol }) => {
+  const [selected, setSelected] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setSelected(false), 600)
+
+    return () => clearTimeout(timer)
+  }, [selected])
+
   return (
     <div 
       onClick={() => {
         navigator.clipboard.writeText(symbol)
+        setSelected(true)
       }}
-      className={styles.emojiBox}
+
+      className={classnames(styles.emojiBox, {
+        [styles.selected]: selected
+      })}
     >
       <p
         className={styles.emoji}
@@ -17,7 +32,9 @@ const EmojiBox = ({ title, symbol }) => {
         }} 
       />
 
-      <p className={styles.emojiText}>{title}</p>
+      <p className={styles.emojiText}>
+        {selected ? 'Copied!' : title}
+      </p>
     </div>
   )
 }
